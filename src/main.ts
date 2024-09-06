3659ea8c5581c8ad8ff5fda2373ed58fae77975a
kamil.furtak <kamil.furtak@gmail.com>
2024-09-06 11:16:58 +0200

    prepare sprite

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,24 @@ const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
 
 const playerImage = new Image();
-playerImage.src = 'assets/player.png';
-let x = 0;
+playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.fillRect(100, 50, 100, 100);
+  // ctx.fillRect(100, 50, 100, 100);
+  ctx.drawImage(
+    playerImage,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight,
+  );
   requestAnimationFrame(animate);
 }
 
